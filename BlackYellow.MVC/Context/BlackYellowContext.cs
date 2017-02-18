1db0f68d7cf8555eedf07dcef47f85f50a313435
walter-lopes <walter_vinicius@outlook.com>
2017-02-18 16:15:04 -0200

    dapper and myql references add

---
+++ BlackYellow.MVC/Context/BlackYellowContext.cs
@@ -0,0 +1,11 @@
+﻿
+using MySql.Data.MySqlClient;  
+
+namespace BlackYellow.MVC.Context
+{
+    public class BlackYellowContext
+    {
+        // criando a conexão com o banco de dados MYSQL
+        public MySqlConnection Connection = new MySqlConnection("Server=localhost;Database=BlackYellowDb; Uid=root;Pwd=root;SslMode=None;");
+    }
+}
---
+++ BlackYellow.MVC/Repositories/RepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace BlackYellow.MVC.Repositories
+{
+    public class RepositoryBase
+    {
+        public Context.BlackYellowContext db;
+
+        //public int Insert()
+        //{
+        //    db.Connection.Insert
+        //}
+    }
+}
