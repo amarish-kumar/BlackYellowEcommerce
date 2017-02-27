04be3cd7fd3a6e6d1d4bf0923c18fb9e81db6239
walter-lopes <walter_vinicius@outlook.com>
2017-02-27 16:50:03 -0300

    user page configured

--- BlackYellow.MVC/wwwroot/js/admin/user/create.js
+++ BlackYellow.MVC/wwwroot/js/admin/user/create.js
@@ -1,24 +1,31 @@
-﻿
-
-
-
-
-function registerUser() {
-
+﻿function registerUser() {   
     var user = {
         Email: $('#email').val(),
         Password: $('#password').val()
     }
 
-    console.log(JSON.stringify(user));
-   
-    $.ajax({
-        type: "POST",
-        contentType : "application/json",
-        url: '/User/RegisterUser',
-        data: JSON.stringify(user),
-        success: function (data) {console.log(data) },
+    if (validaCampos())
+    {
+        $.ajax({
+            type: "POST",
+            contentType : "application/json",
+            url: '/User/RegisterUser',
+            data: JSON.stringify(user),
+            success: function (data) {console.log(data) },
+        });
         
-    });
-
+    }
 };
+
+function validaCampos()
+{
+    if($('#password').val() != $('#confirm_password').val())
+    {       
+        $('#modal-danger').modal();
+        $('.modal-title').html("Alerta")
+        $('.modal-body').html("Senha não estão iguais. Tente novamente");
+        return false;
+    }
+
+    return true;
+}
