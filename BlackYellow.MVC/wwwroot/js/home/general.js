db55223135ca7067e2ac8f35017d47a8ac18e606
walter-lopes <walter_vinicius@outlook.com>
2017-03-12 18:57:59 -0300

    started implementation of page home - products

--- BlackYellow.MVC/wwwroot/js/home/general.js
+++ BlackYellow.MVC/wwwroot/js/home/general.js
@@ -22,7 +22,31 @@ function fillCategories(categories) {
     
 }
 
+function fillProducts(products)
+{
+    var html = "";
 
+    $.each(products, function (item, value) {
+
+        html = '<div class="col-sm-6 col-md-4">' +
+                '<div class="single-product">' +
+                    '<div class="product-f-image">' +
+                       ' <img src="'+value.ImagePrincipal+'" class="img-responsive" alt="">' +
+                        '<div class="product-hover">' +
+                            '<a onclick="buy('+value.ProductId+')" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>' +
+                            '<a href="Product/Details/'+value.ProductId+'" class="view-details-link"><i class="fa fa-link"></i> Detalhes</a>' +
+                        '</div>' +
+                    '</div>' +
+                   ' <h2><a href="Product/Details/' + value.ProductId + '">' + value.Name + '</a></h2>' +
+                   ' <div class="product-carousel-price">' +
+                       ' <ins>R$'+value.LastPrice+'</ins> <del>'+value.Price+'</del>' +
+                   '</div>' +
+               ' </div>' +
+           ' </div>';
+
+        $("#products").append(html);
+    });
+}
 
 
 function getCategories()
@@ -33,6 +57,18 @@ function getCategories()
         url: "/Category/List",
         success: function (data) {
             fillCategories(data.categories);
+        }
+    })
+}
+
+function getProducts()
+{
+    $.ajax({
+        contentType: "application/json",
+        method: "POST",
+        url: "/Product/List",
+        success: function (data) {
+            fillProducts(data.products);
         }
     })
 }
