1f0eabe42b4d203f912de089f1ca28e44e91b4b2
walter-lopes <walter_vinicius@outlook.com>
2017-03-19 18:22:39 -0300

    starting the implementation of cart

--- BlackYellow.MVC/Controllers/HomeController.cs
+++ BlackYellow.MVC/Controllers/HomeController.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using BlackYellow.MVC.Domain.Entites;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace BlackYellow.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        const string SessionCart = "_CART";
         public IActionResult Index()
         {
+            
             return View();
         }
 
@@ -27,6 +32,35 @@ namespace BlackYellow.MVC.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult Order()
+        {
+
+            Cart obj = new Cart();
+            ItemCart item = new ItemCart();
+            item.ItemCartId = 1;
+            item.Price = 200;
+            item.Quantity = 1;
+
+            var strResponse = HttpContext.Session.GetString(SessionCart);
+          
+            obj.Itens = new List<ItemCart>();
+          
+            if (!string.IsNullOrEmpty(strResponse))
+            {
+                var cart = JsonConvert.DeserializeObject<Cart>(strResponse);
+                obj = cart;
+            }
+
+
+            obj.Itens.Add(item);
+
+            var str = JsonConvert.SerializeObject(obj);
+            HttpContext.Session.SetString(SessionCart, str);
+            
+
+            return Json( new { sucesso = HttpContext.Session.GetString(SessionCart) });
         }
 
         public IActionResult Error()
--- BlackYellow.MVC/Controllers/OrderController.cs
+++ BlackYellow.MVC/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,7 @@ namespace BlackYellow.MVC.Controllers
 {
     public class OrderController : Controller
     {
+        const string SessionCart = "_CART";
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -19,6 +21,11 @@ namespace BlackYellow.MVC.Controllers
         public IActionResult Cart()
         {
             return View();
+        }
+
+        public JsonResult Itens()
+        {
+            return Json(new { sucesso = HttpContext.Session.GetString(SessionCart) });
         }
 
         public IActionResult Checkout()
