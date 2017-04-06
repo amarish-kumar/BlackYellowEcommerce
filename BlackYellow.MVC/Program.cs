db878ad78303153e5ba5f9d02985dd393567cceb
walter-lopes <walter_vinicius@outlook.com>
2017-04-06 16:58:47 -0300

    images

--- BlackYellow.MVC/Program.cs
+++ BlackYellow.MVC/Program.cs
@@ -12,7 +12,7 @@ namespace BlackYellow.MVC
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseKestrel()  
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
